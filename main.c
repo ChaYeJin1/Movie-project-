fb63cebe008e44003c06db6975c14ef5c3fee87a
ChaYeJin1 <cha9599831@daum.net>
2018-12-08 01:28:49 +0900

    movie(03)

--- main.c
+++ main.c
@@ -8,7 +8,6 @@
 int main(int argc, char *argv[]) {
 	
 	FILE *fp; //FILE pointer for reading movie data 
-	char buffer[100]; // 파일을 읽을 때 사용할 임시 공간
 	char input [100];
 	int input1;
 	float input2;
@@ -23,15 +22,14 @@ int main(int argc, char *argv[]) {
 	int exit_flag = 0; //flag variable for while loop
 	int option; //user input option
 	int i=0;
-	int word;
 	int arg;
 
 	//2. program start
 	fp=fopen("sample.txt","r+");
 		
     fgets(input, sizeof(input), fp); //sample.txt에서 문자열을 읽음
-            
-    fscanf(fp,"%s %s %d %lf", movies[i].name, movies[i].country, &movies[i].runtime, &movies[i].score);
+       
+    fscanf(fp,"%s %s %d %f", movies[i].name, movies[i].country, &movies[i].runtime, &movies[i].score);
         
     fclose(fp); //파일 포인터 닫기
 
@@ -67,7 +65,7 @@ int main(int argc, char *argv[]) {
 			for(i=0; i<100; i++)
             {
             	printf("runtime : ");
-            	scanf("%s", &movies[i].runtime);
+            	scanf("%d", &movies[i].runtime);
 	        } 
             break;
 				
@@ -75,7 +73,7 @@ int main(int argc, char *argv[]) {
 			for(i=0; i<100; i++)
             {
             	printf("score : ");
-            	scanf("%s", &movies[i].score);
+            	scanf("%f", &movies[i].score);
 	        } 
             break;
 				
