6484fdd7d3bbe46b10381d77bf0b2c4d07e3aedb
ChaYeJin1 <cha9599831@daum.net>
2018-12-11 14:40:48 +0900

    movie(the last&final one)

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX 10 //파일에 들어있는 영화(정보)의 수 
+
 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
@@ -11,7 +13,7 @@ int main(int argc, char *argv[]) {
    struct movie //구조체 정의 
    {
    char name[200]; //영화 제목 
-   char country[10]; //국가 
+   char country[100]; //국가 
    int runtime; //상영 시간 
    float score; //평점 
    };
@@ -28,9 +30,9 @@ int main(int argc, char *argv[]) {
    //2. program start
    fp=fopen("C:\\code\\Movie(project)\\movie(project)\\movie(project)\\Movie-project-\\movie(data).dat","r+"); //영화 정보가 담긴 파일 열기 
        
-   for(i=0;i<10;i++)
+   for(i=0;i<MAX;i++)
    {
-    fscanf(fp,"%s %s %d %f\n", movies[i].name, movies[i].country, &movies[i].runtime, &movies[i].score); //구조체로 받기
+    fscanf(fp,"%s %s %d %f\n", &movies[i].name, &movies[i].country, &movies[i].runtime, &movies[i].score); //구조체로 받기
    }
     fclose(fp); //파일 포인터 닫기
 
@@ -52,7 +54,7 @@ int main(int argc, char *argv[]) {
     case 1: //print all the movies
         printf("\nprinting all the movies in the list.....\n\n\n");
         printf("----------------------------------------\n");
-            for(i=0;i<10;i++)
+            for(i=0;i<MAX;i++)
 	           {
                   printf("Name : %s/country : %s/running time : %d/score : %0.2f\n", movies[i].name, movies[i].country, movies[i].runtime, movies[i].score);
                   printf("----------------------------------------\n");
@@ -63,7 +65,7 @@ int main(int argc, char *argv[]) {
     case 2: //print movies of specific country
         printf("country : ");
         scanf("%s",&key[0].country);//비교할 문자를 입력받는 변수
-            for(j=0;j<10;j++){//저장되어있는 데이터와 입력받은 변수를 비교
+            for(j=0;j<MAX;j++){//저장되어있는 데이터와 입력받은 변수를 비교
                if(!strcmp(movies[j].country,key[0].country))
 			   {//모든 데이터에서 비교하면서 같은 영화정보 출력
                   printf("Name : %s/country : %s/running time : %d/score : %0.2f\n", movies[j].name, movies[j].country, movies[j].runtime, movies[j].score);
@@ -77,7 +79,7 @@ int main(int argc, char *argv[]) {
       case 3: //print movies with long runtime
         printf("runtime : ");
         scanf("%d", &key[0].runtime);//런타임을 입력받음 
-            for(j=0;j<10;j++)
+            for(j=0;j<MAX;j++)
 			{//저장되어있는 데이터와 입력받은 런타임을 비교
                if(movies[j].runtime>=key[0].runtime)
 			   {//입력받은 런타임보다 긴 영화를 출력
@@ -92,7 +94,7 @@ int main(int argc, char *argv[]) {
       case 4: //print movies with high score
         printf("score : ");
         scanf("%f", &key[0].score);
-            for(j=0;j<10;j++){// 평점을 입력받아서 입력받은 평정보다 높은 영화를 출력 
+            for(j=0;j<MAX;j++){// 평점을 입력받아서 입력받은 평정보다 높은 영화를 출력 
                if(movies[j].score>=key[0].score)
 			   {
                   printf("Name : %s/country : %s/running time : %d/score : %0.2f\n", movies[j].name, movies[j].country, movies[j].runtime, movies[j].score);
